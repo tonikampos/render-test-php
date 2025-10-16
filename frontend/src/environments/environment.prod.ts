b15bc11b21648c27bd07e9436d76aeb36bf08415
kampos <kampos@gmail.com>
2025-10-16 19:31:50 +0200

    vamos

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // O enderezo agora é a nosa "entrada do túnel".
-   apiUrl: '/api'
+   apiUrl: 'https://render-test-php-1.onrender.com/api.php'
 };
 //problemas
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // O enderezo tamén é a nosa "entrada do túnel".
-  apiUrl: '/api'
+  apiUrl: 'https://render-test-php-1.onrender.com/api.php'
 };
 //problemas
