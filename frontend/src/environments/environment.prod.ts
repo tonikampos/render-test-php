bf1102e353112558920174502b4467e895d6ad4d
kampos <kampos@gmail.com>
2025-10-16 18:36:17 +0200

    modificado corrs

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // O enderezo agora é a nosa "entrada do túnel".
-   apiUrl: 'https://render-test-php-1.onrender.com/api.php'
+   apiUrl: '/api'
 };
